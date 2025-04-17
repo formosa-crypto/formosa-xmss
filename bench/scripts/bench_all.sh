d7c12775fa80d4556bd66dc8ec2aea79aee0c6a5
Rui Fernandes <ruipedro16@protonmail.com>
2025-04-17 13:15:02 +0200

    fix

--- bench/scripts/bench_all.sh
+++ bench/scripts/bench_all.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+export DISPLAY=:0
+
+notify() {
+    local title="$1"
+    local message="$2"
+    if [ "$(hostname)" = "rui-ThinkPad-T16-Gen-2" ]; then
+        notify-send "$title" "$message" || echo "Failed to send notification"
+    fi
+}
+
 bench_dirs=(
     "hash"
     "wots"
     # "bench/treehash_jasmin_vs_new"
 )
 
-SCRIPT_DIR=$(pwd $(dirname "$0"))
+SCRIPT_DIR=$(dirname "$0")
 
-mkdir -p $SCRIPT_DIR/../bench_results/
+rm -rf "$SCRIPT_DIR/../bench_results/" # remove old results 
+mkdir -p "$SCRIPT_DIR/../bench_results/"
+
+# Check if notify-send is installed
+if ! command -v notify-send &> /dev/null; then
+    echo "notify-send is not installed. Install with sudo apt install libnotify-bin"
+    notify_send_installed=false
+else 
+    notify_send_installed=true
+fi
 
 for dir in "${bench_dirs[@]}"; do
     make -C "$SCRIPT_DIR/../$dir/" "run"
     mv "$SCRIPT_DIR/../$dir/results.txt" "$SCRIPT_DIR/../bench_results/results_$(basename "$dir").txt"
+    
+    if [ "$notify_send_installed" ]; then
+        notify "Benchmark XMSS" "Benchmarking for $dir is done"
+    fi
 done
